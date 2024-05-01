b5732ef66a4f7500e15d52136b49b4829cc4aee4
Arthur Guiot <arguiot@gmail.com>
2024-05-01 15:42:32 -0700

    Added filters

--- components/analytics/PoolTableClient.tsx
+++ components/analytics/PoolTableClient.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import type { ColumnDef } from "@tanstack/react-table";
+import type {
+	ColumnDef,
+	ColumnFiltersState,
+	SortingState,
+} from "@tanstack/react-table";
 import { DataTable } from "../layout/DataTable";
 import { Tabs } from "../ui/tabs";
 import SecurdFormat from "../utils/SecurdFormat";
@@ -11,6 +15,11 @@ import {
 	analyticsToCollateralInfo,
 } from "@/lib/helpers/analytics.helper";
 import MrmScore from "./score";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+import { Input } from "../ui/input";
+import { useState } from "react";
+import { Search } from "lucide-react";
 
 const columns: ColumnDef<PoolTableRows>[] = [
 	{
@@ -79,11 +88,87 @@ const columns: ColumnDef<PoolTableRows>[] = [
 ];
 
 export default function PoolsTableClient({ data }: { data: PoolTableRows[] }) {
+	const [sorting, setSorting] = useState<SortingState>([]);
+	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
+	const toggleSorting = (id: string) => {
+		setSorting((prev) => {
+			const isAlreadySorted = prev.some((s) => s.id === id);
+			if (isAlreadySorted) {
+				return [];
+			}
+			return [{ id, desc: id !== "score" }];
+		});
+	};
+
 	return (
 		<Tabs defaultValue="table" className="text-xl font-bold">
+			<div className="w-full flex flex-col-reverse gap-2 md:gap-0 md:flex-row justify-between my-4">
+				<div className="text-secondary text-sm">
+					Sort by
+					<Button
+						className={cn(
+							"ml-2 rounded-full px-2 h-6",
+							sorting.find((s) => s.id === "tvl")
+								? "bg-securdPrimary text-white"
+								: "bg-secondary",
+						)}
+						onClick={() => toggleSorting("tvl")}
+					>
+						TVL
+					</Button>
+					<Button
+						className={cn(
+							"ml-2 rounded-full px-2 h-6",
+							sorting.find((s) => s.id === "score")
+								? "bg-securdPrimary text-white"
+								: "bg-secondary",
+						)}
+						onClick={() => toggleSorting("score")}
+					>
+						Score
+					</Button>
+					<Button
+						className={cn(
+							"ml-2 rounded-full px-2 h-6",
+							sorting.find((s) => s.id === "apy")
+								? "bg-securdPrimary text-white"
+								: "bg-secondary",
+						)}
+						onClick={() => toggleSorting("apy")}
+					>
+						APY
+					</Button>
+				</div>
+				<div className="relative flex items-center">
+					<Input
+						placeholder="Search"
+						className="pl-8 md:w-48 border-0 border-b rounded-none focus-visible:ring-0 focus-visible:border-b-black focus-visible:border-b-2"
+						value={
+							(columnFilters.find((f) => f.id === "pool")?.value ||
+								"") as string
+						}
+						onChange={(e) => {
+							const value = e.target.value;
+							setColumnFilters((prev) => {
+								const newFilters = prev.filter((f) => f.id !== "pool");
+								if (value) {
+									newFilters.push({ id: "pool", value });
+								}
+								return newFilters;
+							});
+						}}
+					/>
+					<Search className="absolute left-0 w-4 h-4 ml-2" />
+				</div>
+			</div>
 			<DataTable
 				columns={columns}
 				data={data}
+				sorting={sorting}
+				setSorting={setSorting}
+				columnFilters={columnFilters}
+				setColumnFilters={setColumnFilters}
 				linkFn={(row) => `/analytics/${row.id_pool}`}
 			/>
 		</Tabs>
