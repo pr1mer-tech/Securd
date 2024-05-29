ddb7e9afe8c09726a93945fc66a03a2766d29c0b
Arthur Guiot <arguiot@gmail.com>
2024-05-29 14:56:13 -0700

    Using streaming to avoid timeout

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,10 +2,15 @@ import { inngest } from "@/lib/inngest/client";
 import { analyticsRoutine } from "@/lib/inngest/routines/analytics";
 import { serve } from "inngest/next";
 
+// Can be 'nodejs', but Vercel recommends using 'edge'
+export const runtime = 'edge';
+// Prevents this route's response from being cached
+export const dynamic = 'force-dynamic';
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [
         analyticsRoutine,
     ],
+    streaming: "force",
 });
