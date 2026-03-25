b1fa7df66f107825d7d6ec84f89544265bf59c55
Cedric <cedric@smartt.dev>
2026-03-25 20:12:16 +0100

    Disable Biome linter

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useEffect, useState } from "react";
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import SecurdLogo from "@/assets/logos/securd-logo.svg";
