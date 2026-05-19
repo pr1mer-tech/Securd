9f29b2a25a3273b8d61cd6b1e474b25155de521d
Cedric <cedric@smartt.dev>
2026-05-19 11:59:39 +0200

    remove permanent redirect to save page

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  redirects: async () => {
-    return [
-      {
-        source: "/",
-        destination: "/save",
-        permanent: true,
-      },
-    ];
-  },
   webpack: (config) => {
     config.resolve.fallback = {
       fs: false,
